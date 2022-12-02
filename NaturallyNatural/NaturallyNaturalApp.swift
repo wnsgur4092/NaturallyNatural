a2db8d5d15020da7582f82b3527adc4f4ebaf661
Jun <wnsgur4092@gmail.com>
2022-12-02 21:32:37 +1100

    Initial Commit

---
+++ NaturallyNatural/NaturallyNaturalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturallyNaturalApp.swift
+//  NaturallyNatural
+//
+//  Created by JunHyuk Lim on 2/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NaturallyNaturalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
